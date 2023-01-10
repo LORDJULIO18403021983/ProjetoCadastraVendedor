4ea76395730560bf2ef5894aef2eef638dc5741e
Julio Abreu <juliocasp38@gmail.com>
2023-01-09 21:54:04 -0300

    projeto TelaInsereVendedor

--- src/com/br/testaTela/TelaInsereVendedor2.java
+++ src/com/br/testaTela/TelaInsereVendedor2.java
@@ -349,7 +349,7 @@ public class TelaInsereVendedor2 {
             }
 
             listaVendedor.add(vendedor);
-            System.out.println("Exemplo de lista: " + listaVendedor);
+            System.out.println("Exemplo de lista:(refeência) " + listaVendedor);
 
             for (Vendedor s : listaVendedor) {
                 System.out.println("\n Dados do vendedor inseridos com sucesso ===> " + s);
