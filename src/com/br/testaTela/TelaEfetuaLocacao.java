0b277fe01559d2c18366968f93f4b9351550141b
Julio Abreu <juliocasp38@gmail.com>
2023-01-25 12:36:27 -0300

    projeto TelaInsereVendedor

--- src/com/br/testaTela/TelaEfetuaLocacao.java
+++ src/com/br/testaTela/TelaEfetuaLocacao.java
@@ -14,26 +14,6 @@ public class TelaEfetuaLocacao {
 
     private void iniciar() {
 
-        //Texte 04 para Vendedor!!!
-        try {
-            InputStream is = new FileInputStream("cadastraVendedor.txt");
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader leitor = new BufferedReader(isr);
-
-            String texto = leitor.readLine();//Primeira linha.
-
-            while (texto != null) {
-                System.out.println(texto);
-                texto = leitor.readLine();
-            }
-        } catch (FileNotFoundException e) {//Para FileInputStream
-            System.out.println("Arquivo \" cadastraVendedor.txt\" não encontrado.");
-            e.printStackTrace();
-        } catch (IOException e) {//Para o readLine() .
-            System.out.println("Erro na leitura do arquivo de cadastraVendedor");
-            e.printStackTrace();
-        }
-
         //texte 04 para Filme!!!
         try {
             InputStream is = new FileInputStream("cadastraFilme.txt");
@@ -51,6 +31,26 @@ public class TelaEfetuaLocacao {
             e.printStackTrace();
         } catch (IOException e) {//Para o readLine() .
             System.out.println("Erro na leitura do arquivo de cadastraFilme");
+            e.printStackTrace();
+        }
+
+        //Texte 04 para Vendedor!!!
+        try {
+            InputStream is = new FileInputStream("cadastraVendedor.txt");
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader leitor = new BufferedReader(isr);
+
+            String texto = leitor.readLine();//Primeira linha.
+
+            while (texto != null) {
+                System.out.println(texto);
+                texto = leitor.readLine();
+            }
+        } catch (FileNotFoundException e) {//Para FileInputStream
+            System.out.println("Arquivo \" cadastraVendedor.txt\" não encontrado.");
+            e.printStackTrace();
+        } catch (IOException e) {//Para o readLine() .
+            System.out.println("Erro na leitura do arquivo de cadastraVendedor");
             e.printStackTrace();
         }
     }
